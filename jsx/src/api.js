803fd9023ed2ca5a42cbf5b28f399fe79a3220e2
yash1thvs <yashvanthvs.learning@gmail.com>
2024-12-03 18:13:57 +0530

    API integration - project setup - Components creation - Unsplash API - Axios library usage for API call - Async, Await usage - Child to Parent communication (upward communication) - Handling input using state

---
+++ jsx/src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const SearchImages = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: "Client-ID le_cVWwfkFCXvUXxtoV6RdLcKLZa0ZbFbw8ZRGN6i6c"
+        },
+        params: {
+            query: term
+        }
+    });
+    return response.data.results;
+};
+
+export default SearchImages;
